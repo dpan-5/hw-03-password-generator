cbda1a049121825c693cb0b4f719c653e1d6d7a2
David Pan <dpan5425@gmail.com>
2020-09-01 18:11:41 -0400

    created generatePassword() function and initialized required variables

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function writePassword() {
 }
 
 function generatePassword(){
-  //ALL YOUR CODE GOES HERE
+  // Prompts user to select length of string
+  var passwordLength = prompt("Select a string length between 8 and 128 inclusive");
+  // Character types
+  var lowercase = confirm("Include lowercase characters?");
+  var uppercase = confirm("Include uppercase characters?");
+  var numeric = confirm("Include numeric characters?");
+  var specialChars = confirm("Include special characters?");
 }
 
 
